e2038e76f92d2883c129d76f83bbde0d1ef93953
DevFran25 <developerfran25@gmail.com>
2025-07-30 03:20:09 -0500

    lista de amigos y referencias al DOM

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+// Lista de los nombres ingresados
+const listaAmigos = [];
+
+// Elementos 
+const inputNombre = document.getElementById("amigo");
+const listaAmigosHTML = document.getElementById("listaAmigos");
+const resultadoHTML = document.getElementById("resultado");
+
+
+
